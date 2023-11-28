fb8ad6b79cbe8ff32b09228f4058888af8697e1b
miksergeev <99797493+miksergeev@users.noreply.github.com>
2023-11-28 13:28:19 +0300

    Add files via upload

---
+++ asm/lab7/lab7.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+
+int main()
+{
+	int i(0); // i - указатель строки
+
+    std::string s("Hello World!");
+	std::cout << s; // вывод строки
+
+	std::cout << "\nThe string pointer is :\t"; // "указатель равен ... "
+
+	void * firstItem = &s[0]; // передаём подпрограмме указатель на строку
+	__asm {
+		mov eax, firstItem
+		sub eax, 4
+		mov i, eax // в ассемблерной вставке передаём его переменной i
+	}
+	std::cout << std::hex << i;
+	std::cout << "\n";
+	std::cout << &s;
+	getchar();
+}
